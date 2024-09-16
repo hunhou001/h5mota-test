67effb00ae1847c0d5d21abeff5d38696f23e611
qweasz687 <2545567767@qq.com>
2024-09-16 19:00:48 +0800

    解锁功能

--- src/pages/index/App.tsx
+++ src/pages/index/App.tsx
@@ -129,7 +129,7 @@ const App: FC = () => {
                         onClick={() => {
                           const lock = record.disabled ? "锁定" : "解锁"
                           Modal.confirm({ title: '确认框', content: `确认要${lock}这个塔吗？\n锁定后的塔不会再出现在测试员的【我测的塔】列表中。`, onOk: async () => {
-                            requestEditTower({name: record.name, disabled: 1});
+                            requestEditTower({name: record.name, disabled: (record.disabled ? 0 : 1)});
                           }})
                         }}
                       >
